5eef71c967dd6b59fc501cf1066672362e0e5f82
its.Winter <44623320+its-Winter@users.noreply.github.com>
2019-04-03 14:52:48 -0400

    Show you your public IP addresses

--- Shows you your external IP address.bat
+++ Shows you your external IP address.bat
@@ -1,4 +1,8 @@
 @echo off
-echo this is your IP:
-curl ifconfig.co
-pause
+echo This is your external IPv4 address
+curl -4 "ifconfig.co"
+echo ___________________________________
+echo.
+echo This is your external IPv6 address only if full duplex is enabled on your network else this will not resolve host
+curl -6 "ifconfig.co"
+pause >nul

903d40e874784a2272104d1c3d55e20b1eecebb2
its.Winter <44623320+its-Winter@users.noreply.github.com>
2019-05-03 22:28:20 -0400

    Updated - Improved!

--- Shows you your external IP address.bat
+++ Shows you your external IP address.bat
@@ -10,7 +10,10 @@ echo ___________________________________
 echo.
 echo Testing for an external IPv6 address...
 curl -6 --max-time 10 "https://ifconfig.co"2>nul && echo This is your external IPv6 address! You're wise for having one..
-IF %ERRORLEVEL% NEQ 0 echo Looks like you probably don't have IPv6 enabled on your network... && echo or the website is broken with IPv6 currently.
+if %ERRORLEVEL% NEQ 0 (
+    echo Looks like you probably don't have IPv6 enabled on your network... 
+    echo or the website is broken with IPv6 currently.
+)
 echo.
 echo Try again? y / n
 set input=

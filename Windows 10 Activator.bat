b69a888cf99351cad2b22bd07e1b346182d9174a
its.Winter <44623320+its-Winter@users.noreply.github.com>
2019-05-14 10:57:09 -0400

    V1.5 - Checks for version in up-front text

--- Windows 10 Activator.bat
+++ Windows 10 Activator.bat
@@ -45,7 +45,7 @@ echo:- Windows 10 Enterprise LTSB N
 echo:
 echo:
 echo:============================================================================
-echo:Activating your Windows...
+echo: Activating your Windows...
 cscript //nologo slmgr.vbs /ckms >nul
 cscript //nologo slmgr.vbs /upk >nul
 cscript //nologo slmgr.vbs /cpky >nul
@@ -57,15 +57,15 @@ if %errorlevel% EQU 0 ( cscript //nologo slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-
 	cscript //nologo slmgr.vbs /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ >nul
 	cscript //nologo slmgr.vbs /ipk DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ >nul
 	cscript //nologo slmgr.vbs /ipk QFFDN-GRT3P-VKWWX-X7T3R-8B639 >nul
-	goto server) else ( wmic os | findstr /I "home" >nul )
+	goto server) else ( wmic os | find /I "home" >nul || echo You do not have Windows 10 Home... )
 if %errorlevel% EQU 0 ( cscript //nologo slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 >nul
 	cscript //nologo slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM >nul
 	cscript //nologo slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH >nul
 	cscript //nologo slmgr.vbs /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR >nul
-	goto server) else ( wmic os | findstr /I "education" >nul )
+	goto server) else ( wmic os | find /I "education" >nul || echo You do not have Windows 10 Education...)
 if %errorlevel% EQU 0 ( cscript //nologo slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2 >nul
 	cscript //nologo slmgr.vbs /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ >nul
-	goto server) else ( wmic os | findstr /I "10 pro" >nul )
+	goto server) else ( wmic os | find /I "10 pro" >nul || echo You do not have Windows 10 Pro... )
 if %errorlevel% EQU 0 ( cscript //nologo slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX >nul
 	cscript //nologo slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9 >nul
 	goto server) else ( goto notsupported )
@@ -83,7 +83,7 @@ cscript //nologo slmgr.vbs /ato | find /i "successfully" && ( echo:
 	echo:
 	echo:============================================================================
 	choice /n /c yn /m "Would you like to check out my other codes? [ y , n ]?" 
-	if errorlevel 2 exit ) || ( echo:The connection to my KMS server failed! Trying to connect to another one... 
+	if errorlevel GTR 1 exit ) || ( echo:Connection to the KMS server failed! Trying to connect to another one... 
 		echo:Please wait... 
 		echo: 
 		echo: 

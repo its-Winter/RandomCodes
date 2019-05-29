0ef6b1482033596907c96aab5c70f9301a221273
its.Winter <tagrubbsjr@icloud.com>
2019-05-29 09:57:58 -0400

    Fixed some code up

--- Number Guesser.bat
+++ Number Guesser.bat
@@ -1,139 +1,139 @@
 @echo off
 :ABC
 color 0b
-echo think of a number 1-5
-echo.
+echo:think of a number 1-5
+echo:
 pause
 cls
-echo now remember that number or write it down...
-echo.
+echo:now remember that number or write it down...
+echo:
 pause
 cls
-echo answer all of these questions with either yes or no.
-echo.
+echo:answer all of these questions with either yes or no.
+echo:
 pause
 cls
 :startover
 cls
-echo does your number (as a word), start with an "O"?
-echo.
+echo:does your number (as a word), start with an "O"?
+echo:
 set /p answer= : 
 if %answer% == yes goto A
 if %answer% == no goto B
-echo.
-echo Incorrect Input..
+echo:
+echo:Incorrect Input..
 goto startover
 pause
 cls
 :A
-echo is your number "1"?
-echo.
+echo:is your number "1"?
+echo:
 set /p answer= : 
 if %answer% == no goto D
 if %answer% == yes goto C
-echo.
+echo:
 pause
 cls
 :B
-echo does your number (as a word), start with a "T"?
-echo.
+echo:does your number (as a word), start with a "T"?
+echo:
 set /p answer= : 
 if %answer% == no goto E
 if %answer% == yes goto F
-echo.
+echo:
 pause
 cls
 :C
-echo I Won c:
-echo.
+echo:I Won c:
+echo:
 pause
 cls
 goto ABC
 :D
-echo Stop Cheating!
-echo.
+echo:Stop Cheating!
+echo:
 pause
 cls
 goto ABC
 :F
-echo is your number "2"?
-echo.
+echo:is your number "2"?
+echo:
 set /p answer= : 
 if %answer% == no goto G
 if %answer% == yes goto 6
-echo.
+echo:
 pause
 cls
 :E
-echo does your number start with an "F"?
-echo.
+echo:does your number start with an "F"?
+echo:
 set /p answer= : 
 if %answer% == no goto H
 if %answer% == yes goto I
-echo.
+echo:
 pause
 cls
 :G
-echo is your number "3"?
-echo.
+echo:is your number "3"?
+echo:
 set /p answer= : 
 if %answer% == no goto J
 if %answer% == yes goto K
 :J
-echo Stop Cheating!
-echo.
+echo:Stop Cheating!
+echo:
 pause
 cls
 goto ABC
 :H
-echo well something is wrong here...
+echo:well something is wrong here...
 pause
 goto ABC
 :K
-echo I Won c:
-echo.
+echo:I Won c:
+echo:
 pause
 cls
 goto ABC
 :6
-echo I Won c:
-echo.
+echo:I Won c:
+echo:
 pause
 cls
 goto ABC
 :I
-echo is your number "4"?
-echo.
+echo:is your number "4"?
+echo:
 set /p answer= : 
 if %answer% == no goto M
 if %answer% == yes goto L
-echo.
+echo:
 pause
 cls
 :L
-echo I won c:
-echo.
+echo:I won c:
+echo:
 pause
 cls
 goto ABC
 :M
-echo is your number "5"?
-echo.
+echo:is your number "5"?
+echo:
 set /p answer= : 
 if %answer% == no goto N
 if %answer% == yes goto O
-echo.
+echo:
 pause
 cls
 :N
-echo Stop Cheating!
-echo.
+echo:Stop Cheating!
+echo:
 pause
 cls
 goto ABC
 :O
-echo I Won c:
-echo.
+echo:I Won c:
+echo:
 pause
 cls
 goto ABC

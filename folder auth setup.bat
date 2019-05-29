f7db0b94ae418e994724dda8e55f50630bfd0d22
its.Winter <tagrubbsjr@icloud.com>
2019-05-29 09:45:18 -0400

    ok

--- folder auth setup.bat
+++ folder auth setup.bat
@@ -23,82 +23,91 @@ exit /b
 :run
 @echo off
 color 0b
+cd %systemdrive%%homepath%
 title DELETE THIS SETUP WHEN FINISHED!
-del /Q /A:RH "%userprofile%\Documents\config" >nul 2>nul
-del /Q /A:RH "%userprofile%\Documents\config2" >nul 2>nul
+cd %userprofile%
+if exist "Documents\config" do del /Q /A:RH "Documents\config"
+if exist "Documents\config2" do del /Q /A:RH "Documents\config2"
+del /Q /A:RH "Documents\config2" >nul 2>nul
 echo:What do you want your password to be? ~ one word please, or else it doesn't work.
 set /p passwd=: 
-echo:%passwd% > "%userprofile%\Documents\config"
+echo:%passwd% > "Documents\config"
 echo:What folder do you want to lock? 
 echo:example ~ Documents\Secrets
 set /p folder=: 
-echo:%userprofile%\%folder% > "%userprofile%\Documents\config2"
-attrib +H +R "%userprofile%\Documents\config"
-attrib +H +R "%userprofile%\Documents\config2"
+cd %folder%
+for /f %%H in ('cd') do set folder1=%%H
+cd %systemdrive%%homepath%
+echo:%folder1% > "Documents\config2"
+attrib +H +R "Documents\config"
+attrib +H +R "Documents\config2"
 attrib +H %folder%
 echo:Alright, Just a second and everything will be good to go!
 call :writeout
-timeout 8 >nul
-start "%userprofile%\Desktop\FolderAuth.bat" && exit
+timeout 2 >nul
+"Desktop\FolderAuth.bat" && exit
 
 :writeout
-echo:@echo off> %userprofile%\Desktop\FolderAuth.bat
-echo:title Folder Authentication>> %userprofile%\Desktop\FolderAuth.bat
-echo:color 0b>> %userprofile%\Desktop\FolderAuth.bat
-echo:call :isAdmin>> %userprofile%\Desktop\FolderAuth.bat
-echo:if %errorlevel% == 0 goto run>> %userprofile%\Desktop\FolderAuth.bat
-echo:if %errorlevel% NEQ 0 ( echo Requesting Administrative Privileges...>> %userprofile%\Desktop\FolderAuth.bat
-echo:    title Requesting Administrative Privileges...>> %userprofile%\Desktop\FolderAuth.bat
-echo:	goto :UACPrompt )>> %userprofile%\Desktop\FolderAuth.bat
-echo:>> %userprofile%\Desktop\FolderAuth.bat
-echo:exit /b>> %userprofile%\Desktop\FolderAuth.bat
-echo:>> %userprofile%\Desktop\FolderAuth.bat
-echo::UACPrompt>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo Set UAC = CreateObject^^("Shell.Application"^^) ^> "%%temp%%\getadmin.vbs">> %userprofile%\Desktop\FolderAuth.bat
-echo:echo UAC.ShellExecute "cmd.exe", "/c %%~s0 %%~1", "", "runas", 1 ^>^> "%%temp%%\getadmin.vbs">> %userprofile%\Desktop\FolderAuth.bat
-echo:>> %userprofile%\Desktop\FolderAuth.bat
-echo:"%%temp%%\getadmin.vbs">> %userprofile%\Desktop\FolderAuth.bat
-echo:del "%%temp%%\getadmin.vbs">> %userprofile%\Desktop\FolderAuth.bat
-echo:>> %userprofile%\Desktop\FolderAuth.bat
-echo :isAdmin>> %userprofile%\Desktop\FolderAuth.bat
-echo:fsutil dirty query %systemdrive% ^>nul>> %userprofile%\Desktop\FolderAuth.bat
-echo:exit /b>> %userprofile%\Desktop\FolderAuth.bat
-echo:>> %userprofile%\Desktop\FolderAuth.bat
-echo :run >> %userprofile%\Desktop\FolderAuth.bat
-echo:for /f %%%%I in ('more %%userprofile%%\Documents\config') do set password=%%%%I>> %userprofile%\Desktop\FolderAuth.bat
+echo:@echo off> Desktop\FolderAuth.bat
+echo:title Folder Authentication>> Desktop\FolderAuth.bat
+echo:color 0b>> Desktop\FolderAuth.bat
+echo:cls>> Desktop\FolderAuth.bat
+echo:call :isAdmin>> Desktop\FolderAuth.bat
+echo:if %errorlevel% == 0 goto run>> Desktop\FolderAuth.bat
+echo:if %errorlevel% NEQ 0 ( echo Requesting Administrative Privileges...>> Desktop\FolderAuth.bat
+echo:    title Requesting Administrative Privileges...>> Desktop\FolderAuth.bat
+echo:	goto :UACPrompt )>> Desktop\FolderAuth.bat
+echo:>> Desktop\FolderAuth.bat
+echo:exit /b>> Desktop\FolderAuth.bat
+echo:>> Desktop\FolderAuth.bat
+echo::UACPrompt>> Desktop\FolderAuth.bat
+echo:echo Set UAC = CreateObject^^("Shell.Application"^^) ^> "%%temp%%\getadmin.vbs">> Desktop\FolderAuth.bat
+echo:echo UAC.ShellExecute "cmd.exe", "/c %%~s0 %%~1", "", "runas", 1 ^>^> "%%temp%%\getadmin.vbs">> Desktop\FolderAuth.bat
+echo:>> Desktop\FolderAuth.bat
+echo:"%%temp%%\getadmin.vbs">> Desktop\FolderAuth.bat
+echo:del "%%temp%%\getadmin.vbs">> Desktop\FolderAuth.bat
+echo:>> Desktop\FolderAuth.bat
+echo :isAdmin>> Desktop\FolderAuth.bat
+echo:fsutil dirty query %systemdrive% ^>nul>> Desktop\FolderAuth.bat
+echo:exit /b>> Desktop\FolderAuth.bat
+echo:>> Desktop\FolderAuth.bat
+echo :run >> Desktop\FolderAuth.bat
+echo:cd %%systemdrive%%%%homepath%%>> Desktop\FolderAuth.bat
+echo:for /f %%%%I in ('more %%userprofile%%\Documents\config') do set password=%%%%I>> Desktop\FolderAuth.bat
+echo:for /f %%%%G in ('more %%userprofile%%\Documents\config2') do set folder=%%%%G>> Desktop\FolderAuth.bat
 ::Change '\Desktop\Coding\config' to the file is in that contains the super secret password. Also make sure the file is set to read only to prevent any accidental changes to the password and set to hidden.
 ::%userprofile% is C:\Users\<whoever you're logged in as>
 ::For me %userprofile% = C:\Users\Winter -- if you don't know what your %userprofile% variable is set to then open cmd and type
 ::echo %userprofile%
 ::and it will return what it is set to.
-echo :top>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo __________________>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo.>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo Folder Password>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo __________________>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo.>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo You have 1 attempt only.>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo Enter password>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo ______________________________________________>> %userprofile%\Desktop\FolderAuth.bat
-echo:set /p pass= : >> %userprofile%\Desktop\FolderAuth.bat
-echo:if %%pass%% == %%password%% goto correct>> %userprofile%\Desktop\FolderAuth.bat
-echo:if %%pass%% NEQ %%password%% goto fail>> %userprofile%\Desktop\FolderAuth.bat
-echo:cls>> %userprofile%\Desktop\FolderAuth.bat
-echo:goto top>> %userprofile%\Desktop\FolderAuth.bat
-echo :fail>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo Did you forget your own password? Gosh.>> %userprofile%\Desktop\FolderAuth.bat..
-echo:echo Press any key to close window...>> %userprofile%\Desktop\FolderAuth.bat
-echo:pause ^>nul>> %userprofile%\Desktop\FolderAuth.bat
-echo:exit>> %userprofile%\Desktop\FolderAuth.bat
-echo :correct>> %userprofile%\Desktop\FolderAuth.bat
-echo:cls>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo ______________________________________________>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo.>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo Password Accepted!>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo Opening Folder...>> %userprofile%\Desktop\FolderAuth.bat
-echo:echo ______________________________________________>> %userprofile%\Desktop\FolderAuth.bat
+echo :top>> Desktop\FolderAuth.bat
+echo:echo __________________>> Desktop\FolderAuth.bat
+echo:echo.>> Desktop\FolderAuth.bat
+echo:echo Folder Password>> Desktop\FolderAuth.bat
+echo:echo __________________>> Desktop\FolderAuth.bat
+echo:echo.>> Desktop\FolderAuth.bat
+echo:echo You have 1 attempt only.>> Desktop\FolderAuth.bat
+echo:echo Enter password>> Desktop\FolderAuth.bat
+echo:echo ______________________________________________>> Desktop\FolderAuth.bat
+echo:set /p pass= : >> Desktop\FolderAuth.bat
+echo:if %%pass%% == %%password%% goto correct>> Desktop\FolderAuth.bat
+echo:if %%pass%% NEQ %%password%% goto fail>> Desktop\FolderAuth.bat
+echo:cls>> Desktop\FolderAuth.bat
+echo:goto top>> Desktop\FolderAuth.bat
+echo :fail>> Desktop\FolderAuth.bat
+echo:echo Did you forget your own password? Gosh.>> Desktop\FolderAuth.bat..
+echo:echo Press any key to close window...>> Desktop\FolderAuth.bat
+echo:pause ^>nul>> Desktop\FolderAuth.bat
+echo:exit>> Desktop\FolderAuth.bat
+echo :correct>> Desktop\FolderAuth.bat
+echo:cls>> Desktop\FolderAuth.bat
+echo:echo ______________________________________________>> Desktop\FolderAuth.bat
+echo:echo.>> Desktop\FolderAuth.bat
+echo:echo Password Accepted!>> Desktop\FolderAuth.bat
+echo:echo Opening Folder...>> Desktop\FolderAuth.bat
+echo:echo ______________________________________________>> Desktop\FolderAuth.bat
 ::Also change this directory to the folder you're trying to need authentication for. TIP: Make the folder hidden
-echo:explorer %folder%>> %userprofile%\Desktop\FolderAuth.bat
-echo:ping localhost -n 2 ^>nul>> %userprofile%\Desktop\FolderAuth.bat
-echo:exit>> %userprofile%\Desktop\FolderAuth.bat
+echo:explorer %folder%>> Desktop\FolderAuth.bat
+echo:ping localhost -n 2 ^>nul>> Desktop\FolderAuth.bat
+echo:exit>> Desktop\FolderAuth.bat
 exit /b
---
+++ information.bat
@@ -0,0 +1,152 @@
+@echo off
+title Info
+color 0c
+:mainmenu
+cls
+echo:Whatcha need?
+echo:[1] Basic Information
+echo:[2] Network information
+echo:[3] System Information
+echo:[4] Advanced Information
+echo:[5] DEV
+echo:[6] Exit this script
+echo:----------------------------------
+set /p input=: 
+if %input%==1 goto :Basic
+if %input%==2 goto :Net
+if %input%==3 goto :System
+if %input%==4 goto :Advanced
+if %input%==5 goto :Devstate
+if %input%==6 ( exit
+) else ( 
+    echo: invalid reponse try again & goto :mainmenu
+)
+
+:Basic
+set currentlabel=:Basic
+echo:----------------------------------
+echo:
+echo:You are logged in as...
+echo:%username% || echo:Couldn't find it.. Error 404
+echo:
+echo:----------------------------------
+echo:
+echo:Your System Timezone is...
+wmic timezone get description | find "(" || echo:Couldn't find it.. Error 404
+echo:----------------------------------
+echo:
+echo Your PC's name is...
+echo:%computername% || echo:Couldn't find it.. Error 404
+echo:
+echo:----------------------------------
+echo:
+echo:Windows Information
+wmic os get caption,version,buildnumber,serialnumber
+echo:----------------------------------
+goto :tryagain
+
+:Net
+set currentlabel=:Net
+cls
+echo:----------------------------------
+echo:
+echo:Your internal IPv4 address(es) is(are)...
+ipconfig | find "IPv4 Address" || echo:Couldn't find it.. Error 404
+echo:
+echo:----------------------------------
+echo:
+echo Your external IPv4 address is...
+curl -4 "https://ifconfig.co" || echo:Either the way this works is broken or this just failed...
+echo:
+echo:----------------------------------
+echo:
+echo Your external IPv6 address is...
+curl -6 "https://ifconfig.co" || ( echo:Looks like you don't have IPv6 on your external...
+    echo: For nerds looks like you don't have full duplex enabled on WAN...
+)
+echo:
+echo:----------------------------------
+goto :tryagain
+
+:System
+set currentlabel=:System
+echo:----------------------------------
+echo:
+echo:CPU Identification
+wmic cpu get name | find "I" || wmic cpu get name | find "A" || echo:Couldn't find it.. Error 404
+echo:
+echo:----------------------------------
+echo:
+echo CPU Core Count
+echo:%NUMBER_OF_PROCESSORS% || echo:Couldn't find it.. Error 404
+echo:
+echo:----------------------------------
+echo:
+echo:CPU Architecture
+wmic computersystem get systemtype | find "x" || echo:Couldn't find it.. Error 404
+echo:
+echo:----------------------------------
+echo:
+echo:Total Amount of System Memory
+wmic os get TotalVisibleMemorySize | find "1" || wmic os get TotalVisibleMemorySize | find "2" || wmic os get TotalVisibleMemorySize | find "3" ||  wmic os get TotalVisibleMemorySize | find "4" ||  wmic os get TotalVisibleMemorySize | find "5" ||  wmic os get TotalVisibleMemorySize | find "6" ||  wmic os get TotalVisibleMemorySize | find "7" ||  wmic os get TotalVisibleMemorySize | find "8" ||  wmic os get TotalVisibleMemorySize | find "9" ||  echo:Couldn't find it.. Error 404
+echo:
+echo:----------------------------------
+goto :tryagain
+
+:Advanced
+set currentlabel=:Advanced
+echo:----------------------------------
+echo:
+:: LOOK INTO WMIC LOGICALDISK
+echo:Disk Volumes
+wmic partition get name,size,type
+echo:----------------------------------
+echo:
+echo:BIOS Information
+wmic bios get caption,manufacturer
+echo:----------------------------------
+echo:
+echo:Boot Type
+wmic computersystem get bootupstate | find "N" || wmic computersystem get bootupstate | find "F"
+echo:
+echo:----------------------------------
+echo:
+echo:Motherboard Information
+wmic baseboard get manufacturer,product,serialnumber,status
+echo:----------------------------------
+
+:Devstate
+set currentlabel=:Devstate
+echo:----------------------------------
+echo:I will need to export this to a text file since there are 
+echo:too many drivers to show in this cmd session...
+echo:Continue? [y, n]
+set /p continue=: 
+if %continue%==n goto :mainmenu
+if %continue%==y ( cd %userprofile%
+    call :driveroutput >Desktop\Driveroutput.txt 
+) else ( 
+    echo:Invalid response try again & goto :Devstate 
+)
+
+:tryagain
+echo:
+echo:Where to now?
+echo:[1] Go to main menu
+echo:[2] Show this page again
+echo:[3] Exit the script
+echo:----------------------------------
+set /p input2=: 
+if %input2%==1 goto :mainmenu
+if %input2%==2 goto %currentlabel%
+if %input2%==3 ( exit 
+) else ( echo:Invalid reponse try again 
+    goto :tryagain
+)
+
+:driveroutput
+echo:----------------------------------
+echo:
+echo:All drivers installed on your machine are...
+wmic sysdriver get caption
+echo:----------------------------------

a8793eaa3fbcc9a15d2b45910d68a3575a32d5ad
its.Winter <tagrubbsjr@icloud.com>
2019-09-11 09:07:44 -0400

    fixed 'echo'

--- Shows you your external IP address.bat
+++ Shows you your external IP address.bat
@@ -1,21 +1,21 @@
 @echo off
 goto start
 :startover
-echo.
-echo.
+echo:
+echo:
 :start
-echo This is your external IPv4 address
+echo:This is your external IPv4 address
 curl -4 "https://ifconfig.co"
-echo ___________________________________
-echo.
-echo Testing for an external IPv6 address...
-curl -6 --max-time 10 "https://ifconfig.co"2>nul && echo This is your external IPv6 address! You're wise for having one..
+echo:___________________________________
+echo:
+echo:Testing for an external IPv6 address...
+curl -6 --max-time 10 "https://ifconfig.co"2>nul && echo:This is your external IPv6 address! You're wise for having one..
 if %ERRORLEVEL% NEQ 0 (
-    echo Looks like you probably don't have IPv6 enabled on your network... 
-    echo or the website is broken with IPv6 currently.
+    echo:Looks like you probably don't have IPv6 enabled on your network... 
+    echo:or the website is broken with IPv6 currently.
 )
-echo.
-echo Try again? y / n
+echo:
+echo:Try again? [ y / n ]
 set /p input=
 if %input% == y goto startover
 if %input% == n exit

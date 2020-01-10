a6a72a3cdf56aeca402b1b0c8e14fe7b813cbe43
its.Winter <44623320+its-Winter@users.noreply.github.com>
2020-01-09 22:01:23 -0500

    Create Classic Matrix.ps1

---
+++ Powershell/Classic Matrix.ps1
@@ -0,0 +1,5 @@
+$host.ui.RawUI.WindowTitle = "You're a wizard harry"
+while ($true) {
+      "$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)
+$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)$(Get-Random -Minimum 10000000000)"
+}
